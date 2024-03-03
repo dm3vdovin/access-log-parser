7556b91062fb9d9683a230a59ec36b4e5594a79e
Dmitry Vdovin <dm3vdovin@gmail.com>
2024-03-03 22:28:09 +0300

    Добавлен код для выполнения операций с числами

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        int amount = firstNumber + secondNumber;
+        System.out.println("Сумма чисел равна: " + amount);
+        int difference = firstNumber - secondNumber;
+        System.out.println("Разность чисел равна: " + difference);
+        int multiplication = firstNumber * secondNumber;
+        System.out.println("Произведение чисел равно: " + multiplication);
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Частное от деления первого числа на второе равно: " + quotient);
     }
 }
