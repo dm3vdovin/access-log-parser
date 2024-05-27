1a0857830cdeb32a2f07b726f29329615fd68968
Dmitry Vdovin <dm3vdovin@gmail.com>
2024-05-27 20:22:54 +0300

    Добавлен подсчёт доли запросов от YandexBot и GoogleBot.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 import java.io.*;
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         int filesCount = 1;
         int totalLinesCount = 0;
-        int maxLength = 0;
-        int minLength = Integer.MAX_VALUE;
         int lineLengthLimit = 1024;
+        int botTotalCountGoogle = 0;
+        int botTotalCountYandex = 0;
+        String botNameGoogle = "Googlebot";
+        String botNameYandex = "YandexBot";
 
         while (true) {
             System.out.println("Укажите абсолютный путь к файлу логов: ");
@@ -37,19 +42,53 @@ public class Main {
                     if (length > lineLengthLimit) {
                         throw new ExceedMaxLengthException("В файле встретилась строка длиннее " + lineLengthLimit + " символов");
                     }
-                    if (length > maxLength) {
-                        maxLength = length;
-                    }
-                    if (length < minLength) {
-                        minLength = length;
+
+                    Pattern regexPattern = Pattern.compile(
+                            "(?<ipAdress>\\d+.\\d+.\\d+.\\d+) " +
+                                    "(?<property1>.*?) " +
+                                    "(?<property2>.*?) " +
+                                    "(?<timeStamp>.*?) " +
+                                    "(?<requestMethod>.*) " +
+                                    "(?<responseCode>\\d+?) " +
+                                    "(?<dataSize>\\d+?) " +
+                                    "(?<pageUrl>.*?) " +
+                                    "(?<userAgent>.*?)");
+
+                    Matcher userAgentMatcher = regexPattern.matcher(line);
+
+                    if (userAgentMatcher.matches()) {
+                        String userAgent = userAgentMatcher.group("userAgent");
+                        int index = userAgent.indexOf("(compatible;");
+                        String botDataBrackets = userAgent.substring(index + 1);
+
+                        String[] parts = botDataBrackets.split(";");
+
+                        if (parts.length >= 2) {
+                            String fragment = parts[1];
+                            parts = fragment.split("/");
+                            String botName = parts[0].trim();
+
+                            if (botName.equals(botNameYandex)) {
+                                botTotalCountYandex++;
+                            }
+
+                            if (botName.equals(botNameGoogle)) {
+                                botTotalCountGoogle++;
+                            }
+                        }
+
                     }
 
                     totalLinesCount++;
+
                 }
 
                 System.out.println("Общее количество строк в файле: " + totalLinesCount);
-                System.out.println("Длина самой длинной строки в файле: " + maxLength);
-                System.out.println("Длина самой короткой строки в файле: " + minLength);
+                DecimalFormat formatter = new DecimalFormat("#0.00");
+                double botYandexContribution = (double) botTotalCountYandex / totalLinesCount * 100;
+                double botGoogleContribution = (double) botTotalCountGoogle / totalLinesCount * 100;
+                System.out.println("Доля запросов от YandexBot относительно общего числа запросов: " + formatter.format(botYandexContribution) + " (" + botTotalCountYandex + ")");
+                System.out.println("Доля запросов от GoogleBot относительно общего числа запросов: " + formatter.format(botGoogleContribution) + " (" + botTotalCountGoogle + ")");
 
             } catch (FileNotFoundException ex) {
                 throw new ExceedMaxLengthException("Указанный файл не найден");
