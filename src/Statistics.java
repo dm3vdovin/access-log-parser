dae71c4c1ee61f7414dc9913a50c040c0ccf1ebf
Dmitry Vdovin <dm3vdovin@gmail.com>
2024-05-28 21:23:09 +0300

    Добавлена возможность вывода списка несуществующих страниц, доли браузеров.

--- src/Statistics.java
+++ src/Statistics.java
@@ -6,17 +6,23 @@ import java.util.HashSet;
 public class Statistics {
     private long totalTraffic;
     private LocalDateTime minTime, maxTime;
-    private final HashSet<String> pageLinks;
+    private final HashSet<String> existingPageLinks;
+    private final HashSet<String> nonExistingPageLinks;
     private final HashMap<String, Integer> oSLoggingFrequency;
+    private final HashMap<String, Integer> browserLoggingFrequency;
     int successCode = 200;
+    int notFoundCode = 404;
     private int osCount = 0;
+    private int browserCount = 0;
 
     public Statistics() {
         this.totalTraffic = 0;
         this.minTime = LocalDateTime.now();
         this.maxTime = LocalDateTime.now();
-        this.pageLinks = new HashSet<>();
+        this.existingPageLinks = new HashSet<>();
+        this.nonExistingPageLinks = new HashSet<>();
         this.oSLoggingFrequency = new HashMap<>();
+        this.browserLoggingFrequency = new HashMap<>();
     }
 
     public void addEntry(LogEntry logEntry) {
@@ -30,56 +36,55 @@ public class Statistics {
             maxTime = logEntry.getTimeStamp();
         }
 
-        getPageLinks(logEntry);
-        getOsLoggingFrequencyRate(logEntry);
-
+        getPageLinks(logEntry, existingPageLinks, successCode);
+        getPageLinks(logEntry, nonExistingPageLinks, notFoundCode);
+        getLoggingFrequencyRate(logEntry, oSLoggingFrequency, osCount);
+        getLoggingFrequencyRate(logEntry, browserLoggingFrequency, browserCount);
     }
 
     public double getTrafficRate() {
         Duration timeDuration = Duration.between(minTime, maxTime);
         double differenceInHours = timeDuration.toHours();
+
         return (double) totalTraffic / differenceInHours;
     }
 
-    public HashSet<String> getPageLinks(LogEntry logEntry) {
+    public HashSet<String> getPageLinks(LogEntry logEntry, HashSet<String> pageLinks, int statusCode) {
         String pageLink = logEntry.getRequestPath();
 
-        if (logEntry.getResponseCode() == successCode) {
+        if (logEntry.getResponseCode() == statusCode) {
             pageLinks.add(pageLink);
         }
 
         return pageLinks;
     }
 
-    public HashMap<String, Double> getOsLoggingFrequencyRate(LogEntry logEntry) {
+    public HashMap<String, Double> getLoggingFrequencyRate(LogEntry logEntry, HashMap<String, Integer> loggingFrequency, int count) {
         UserAgent userAgent = new UserAgent(logEntry.getUserAgent());
         String userAgentString = logEntry.getUserAgent();
 
         if (userAgent.getOsName(userAgentString) != null) {
-            String oSName = userAgent.getOsName(userAgentString);
+            String name = userAgent.getOsName(userAgentString);
 
-            if (oSLoggingFrequency.containsKey(oSName)) {
-                oSLoggingFrequency.put(oSName, oSLoggingFrequency.get(oSName) + 1);
+            if (loggingFrequency.containsKey(name)) {
+                loggingFrequency.put(name, loggingFrequency.get(name) + 1);
             } else {
-                oSLoggingFrequency.put(oSName, 1);
+                loggingFrequency.put(name, 1);
             }
-
         }
 
         HashMap<String, Double> oSLoggingFrequencyRate = new HashMap<>();
 
-        for (String key : oSLoggingFrequency.keySet()) {
-            osCount += oSLoggingFrequency.get(key);
-            double value = (double) oSLoggingFrequency.get(key) / osCount;
+        for (String key : loggingFrequency.keySet()) {
+            count += loggingFrequency.get(key);
+            double value = (double) loggingFrequency.get(key) / count;
             oSLoggingFrequencyRate.put(key, value);
         }
 
-        for (Integer value : oSLoggingFrequency.values()) {
-            osCount += value;
+        for (Integer value : loggingFrequency.values()) {
+            count += value;
         }
 
         return oSLoggingFrequencyRate;
     }
 }
-
-
